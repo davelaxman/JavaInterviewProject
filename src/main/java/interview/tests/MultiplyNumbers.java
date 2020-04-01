82a0a1894184f768770d4040cae506520bc8e3bf
David Laxman <davelaxman@Davids-MacBook-Pro.local>
2020-04-01 14:52:57 -0600

    Adding in Multiply Numbers

---
+++ src/main/java/interview/tests/MultiplyNumbers.java
@@ -0,0 +1,12 @@
+public class MultiplyNumbers {
+    public static void main(String args[]){
+        System.out.println("Expected: "+ (202*13)+" Result: "+MultipleNumbers(202,13));
+    }
+
+    //Please multiple two numbers without using the multiplication operator
+    public static int MultipleNumbers(int number1, int number2) {
+        int result = 0;
+        // Add your code here
+        return result;
+    }
+}
